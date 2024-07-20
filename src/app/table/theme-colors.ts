1ff08a12008c93bcfa44751d2d6e7fb19aa15cc9
vika-v <vika.vovchenkoo@gmail.com>
2024-07-20 12:26:15 +0200

    added futher styles

--- src/app/table/theme-colors.ts
+++ src/app/table/theme-colors.ts
@@ -1,7 +1,7 @@
 export const themeColors = {
   "light" : {
     "background" : "#eeeeee",
-    "second-background" : "#eef0f2",
+    "second-background" : "#fefefe",
     "table-header" : "#e1e1e1",
     "border-color": "#aeaeae",
     "text" : "#1c1c1c"
@@ -9,8 +9,8 @@ export const themeColors = {
   "dark" : {
     "background" : "#252525",
     "second-background" : "#2e2e2e",
-    "table-header" : "#4b4b4b",
-    "border-color": "#bdbec0",
+    "table-header" : "#3a3a3a",
+    "border-color": "#606060",
     "text" : "#eef0ef"
   }
 };
