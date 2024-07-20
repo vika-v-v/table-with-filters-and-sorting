90821728f0844858f241a09f05520f8fecab4989
vika-v <vika.vovchenkoo@gmail.com>
2024-07-20 14:27:37 +0200

    added theme to dropdown

--- src/app/custom-dropdown/theme-colors.ts
+++ src/app/custom-dropdown/theme-colors.ts
@@ -1,12 +1,14 @@
 export const themeColors = {
   "light" : {
     "default-color" : "#868686",
-    "hovered-color" : "#e1e1e1",
+    "options-container-color" : "#acacac",
+    "hovered-color" : "#c2c2c2",
     "border-color": "#aeaeae",
     "text" : "#1c1c1c"
   },
   "dark" : {
     "default-color" : "#6c6c6c",
+    "options-container-color" : "#6c6c6c",
     "hovered-color" : "#464646",
     "border-color": "#606060",
     "text" : "#eef0ef"
